3c907d7f06289517d482303a84e71ee48f5c9f97
Arne Döring <arne.doering@gmx.net>
2011-07-09 16:08:51 +0200

    migration patch, remove of old build files

--- project/plugins/Plugin.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-class Plugin(info: ProjectInfo) extends PluginDefinition(info) {
-	val lwjglVersion = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.4"
-	val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
-	val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
-}
-
