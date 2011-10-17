438b97973212565a96355dc53dcee974d4702d62
Arne Doering <arne.doering@gmx.net>
2011-10-17 21:24:14 +0200

    QuickHull3d Angefangen

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -94,20 +94,20 @@ object Util {
 	import scala.collection.generic.CanBuildFrom
 
 	class RichCollection[A, Repr](xs: IterableLike[A, Repr]){
-	  def distinctBy[B, That](f: A => B)(implicit cbf: CanBuildFrom[Repr, A, That]) = {
-	    val builder = cbf(xs.repr)
-	    val i = xs.iterator
-	    var set = Set[B]()
-	    while(i.hasNext) {
-	      val o = i.next
-	      val b = f(o)
-	      if (!set(b)) {
-	        set += b
-	        builder += o
-	      }
-	    }
-	    builder.result
-	  }
+		def distinctBy[B, That](f: A => B)(implicit cbf: CanBuildFrom[Repr, A, That]) = {
+			val builder = cbf(xs.repr)
+			val i = xs.iterator
+			var set = Set[B]()
+			while(i.hasNext) {
+				val o = i.next
+				val b = f(o)
+				if (!set(b)) {
+					set += b
+					builder += o
+				}
+			}
+			builder.result
+		}
 	}
 
 	implicit def toRich[A, Repr](xs: IterableLike[A, Repr]) = new RichCollection(xs)
