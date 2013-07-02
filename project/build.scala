4a6db95689842ea0b21920a8f8c567663950ce54
Felix Dietze <felix@beerleader.net>
2013-07-02 17:56:06 +0200

    simple benchmark infrastructure
    run
    
    $sbt benchmark
    
    to execute downearth.benchmark.Main

--- project/build.scala
+++ project/build.scala
@@ -1,6 +1,16 @@
 import sbt._
 import scalabuff.ScalaBuffPlugin._
+import Keys._
 
 object build extends Build {
-  lazy val root = Project("main", file("."), settings = Defaults.defaultSettings ++ scalabuffSettings).configs(ScalaBuff)
+  lazy val root = Project(
+    "main",
+    file("."),
+    settings = Defaults.defaultSettings ++ scalabuffSettings ++ Seq(benchmarkTask)
+    ).configs(ScalaBuff)
+  
+
+  val benchmark = TaskKey[Unit]("benchmark")
+  benchmark <<= benchmark.dependsOn(compile)
+  val benchmarkTask = fullRunTask(benchmark, Test, "downearth.benchmark.Main")
 }
---
+++ src/main/scala/downearth/benchmark/benchmark.scala
@@ -0,0 +1,5 @@
+package downearth.benchmark
+
+object Main extends App {
+  println("benchmarking!")
+}
