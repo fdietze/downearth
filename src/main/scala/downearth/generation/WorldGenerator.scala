d49b8dad20e337af3c7615ac9d5509e0beb7ef34
Felix Dietze <felix@beerleader.net>
2013-07-03 17:07:22 +0200

    reindent genWorldAt

--- src/main/scala/downearth/generation/WorldGenerator.scala
+++ src/main/scala/downearth/generation/WorldGenerator.scala
@@ -32,58 +32,58 @@ object WorldGenerator {
 		octree
 	}
 
-	def genWorldAt(nodeInfo:NodeInfo,
+  def genWorldAt(nodeInfo:NodeInfo,
                  worldFunction:WorldFunction = WorldDefinition,
                  deltaSetFuture:Future[message.DeltaSet] = Future{message.DeltaSet()}):NodeOverMesh = {
     val NodeInfo(nodepos, nodesize) = nodeInfo
-		import HexaederMC._
-		//TODO: val toSample = findNodesToSample(nodeInfo, worldFunction)
+    import HexaederMC._
+    //TODO: val toSample = findNodesToSample(nodeInfo, worldFunction)
 
-		// Braucht eine zusätzliche größe um 2 damit die Nachbarn besser angrenzen können
-		// Marching-Cubes für n Cubes: n+1 Datenpunkte
-		// Für Umrandungen: n+2 Cubes mit n+3 Datenpunkten
-		val originalNoiseData = new Array3D[Double](Vec3i(nodesize+3))
-		// Füllen der Datenpunkte mit Hilfe der Dichtefunktion
-		originalNoiseData.fill(v =>	worldFunction.density(Vec3(nodepos+v-1)))
-		val modifiedNoiseData = originalNoiseData.clone
-		val exactCaseData = new Array3D[Short](Vec3i(nodesize+2))
-	
+    // Braucht eine zusätzliche größe um 2 damit die Nachbarn besser angrenzen können
+    // Marching-Cubes für n Cubes: n+1 Datenpunkte
+    // Für Umrandungen: n+2 Cubes mit n+3 Datenpunkten
+    val originalNoiseData = new Array3D[Double](Vec3i(nodesize+3))
+    // Füllen der Datenpunkte mit Hilfe der Dichtefunktion
+    originalNoiseData.fill(v =>	worldFunction.density(Vec3(nodepos+v-1)))
+    val modifiedNoiseData = originalNoiseData.clone
+    val exactCaseData = new Array3D[Short](Vec3i(nodesize+2))
 
-		// Fall für jeden Cube ermitteln und abspeichern
-		for( coord <- Vec3i(0) until Vec3i(nodesize+2) ) {
-			val exactCase = dataToCase(originalNoiseData.extract(coord))
-			exactCaseData(coord) = exactCase.toShort
-		}
-	
-		// für jeden Cube:
-		for( coord <- Vec3i(0) until Vec3i(nodesize+2) ) {
-			// Datenpunkte extrahieren
-			val originalData = originalNoiseData.extract(coord)
-			val modifiedData = modifiedNoiseData.extract(coord)
 
-			// Fall für diesen Cube auslesen und benennen
-			val exactCase = exactCaseData(coord)
-			val caseType = caseTypeLookup(exactCase)
-	
-			// Wenn Fall nicht darstellbar
-			if( !isStableCase(caseType) ) {
-				// In einen darstellbaren Fall transformieren
-				val (newData, newCase) = transformToStable(originalData, exactCase)
-				
-				// Stabilisierung auf die schon modifizierten Datan anwenden
-				val merge = 
-					for( i <- 0 until 8 ) yield {
-						if( newData(i) == 0 )
-							0
-						else
-							modifiedData(i)
-					}
-				
-				// Transformierten Cube abspeichern
-				modifiedNoiseData(coord) = merge
-				exactCaseData(coord) = newCase.toShort
-			}
-		}
+    // Fall für jeden Cube ermitteln und abspeichern
+    for( coord <- Vec3i(0) until Vec3i(nodesize+2) ) {
+      val exactCase = dataToCase(originalNoiseData.extract(coord))
+      exactCaseData(coord) = exactCase.toShort
+    }
+
+    // für jeden Cube:
+    for( coord <- Vec3i(0) until Vec3i(nodesize+2) ) {
+      // Datenpunkte extrahieren
+      val originalData = originalNoiseData.extract(coord)
+      val modifiedData = modifiedNoiseData.extract(coord)
+
+      // Fall für diesen Cube auslesen und benennen
+      val exactCase = exactCaseData(coord)
+      val caseType = caseTypeLookup(exactCase)
+
+      // Wenn Fall nicht darstellbar
+      if( !isStableCase(caseType) ) {
+        // In einen darstellbaren Fall transformieren
+        val (newData, newCase) = transformToStable(originalData, exactCase)
+
+        // Stabilisierung auf die schon modifizierten Datan anwenden
+        val merge = 
+        for( i <- 0 until 8 ) yield {
+          if( newData(i) == 0 )
+            0
+          else
+            modifiedData(i)
+        }
+
+        // Transformierten Cube abspeichern
+        modifiedNoiseData(coord) = merge
+        exactCaseData(coord) = newCase.toShort
+      }
+    }
 
     // wait for world d from server to overwrite generated blocks
     val deltaSet = Await.result(deltaSetFuture, Timeout(5 seconds).duration).asInstanceOf[message.DeltaSet]
@@ -94,20 +94,20 @@ object WorldGenerator {
 
 
     // Liest die abgespeicherten Fälle aus und erzeugt entsprechende Hexaeder
-    // Berücksichtigt auch gespeicherte User-Ändarungen
-		def fillfun(v:Vec3i) = {
-			val arraypos = v + 1 - nodepos
-			val h = data2hexaeder( modifiedNoiseData.extract(arraypos), exactCaseData(arraypos) )
+    // Berücksichtigt auch gespeicherte User-Änderungen vom Server
+    def fillfun(v:Vec3i) = {
+    val arraypos = v + 1 - nodepos
+    val h = data2hexaeder( modifiedNoiseData.extract(arraypos), exactCaseData(arraypos) )
 
-      val delta = deltaMap.get(v)
-      if( delta.isDefined ) {
-        Hexaeder.fromMessage(delta.get.shape)
-      } else
-        if( h.noVolume )
-          EmptyHexaeder
-        else
-          h
-		}
+    val delta = deltaMap.get(v)
+    if( delta.isDefined ) {
+      Hexaeder.fromMessage(delta.get.shape)
+    } else
+      if( h.noVolume )
+      EmptyHexaeder
+      else
+      h
+    }
 
     // Octree mit Hexaedern füllen
     // TODO use prediction here
@@ -116,7 +116,7 @@ object WorldGenerator {
     root.genMesh( nodeInfo, minMeshNodeSize, x => {
       if (nodeInfo.indexInRange(x)) root(nodeInfo, x).h else fillfun(x)
     } )
-	}
+  }
 
 
   // Ask server for World delta asynchronly
